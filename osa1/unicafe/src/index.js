91689ca54f3400f4cc8c09c8b76ad1860a2e20f1
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-06-09 19:55:10 +0300

    Tehtävä 1.11 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,23 +11,26 @@ const Button = ({handleClick, text}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <div>
-     {text} {value}
-    </div>
+      <tr>
+        <td>{text}</td>
+	<td>{value}</td>
+      </tr>
   )
 }
 
 const Statistics = ({good, neutral, bad}) => {
   if (good || neutral || bad > 0){
     return (
-       <div>
-        <StatisticLine text={"good"} value={good}/> 
-        <StatisticLine text={"neutral"} value={neutral}/> 
-        <StatisticLine text={"bad"} value={bad}/>
-        <StatisticLine text={"all"} value={good + bad + neutral}/>
-	<StatisticLine text={"average"} value={(good - bad)/(good + bad + neutral)}/>
-	<StatisticLine text={"positive"} value={(good/(good + bad + neutral)*100)}/>
-        </div>
+       <table>
+         <tbody>
+           <StatisticLine text={"good"} value={good}/> 
+           <StatisticLine text={"neutral"} value={neutral}/> 
+           <StatisticLine text={"bad"} value={bad}/>
+           <StatisticLine text={"all"} value={good + bad + neutral}/>
+	   <StatisticLine text={"average"} value={(good - bad)/(good + bad + neutral)}/>
+	   <StatisticLine text={"positive"} value={(good/(good + bad + neutral)*100)}/>
+         </tbody>
+       </table>
 
     )
 		

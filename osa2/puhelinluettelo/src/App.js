8680ffa5fd1fd1f0852616a2415b4580ce054185
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-06-23 20:19:45 +0300

    Tehtävä 2.7 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -9,6 +9,11 @@ const App = () => {
   
     const addName = (event) => {
         event.preventDefault()
+        if (persons.some(person => person.name === newName)){
+            window.alert(`${newName} is already added to phonebook`)
+            setNewName('')
+            return
+        }
         const personObject = {
             name: newName
         }

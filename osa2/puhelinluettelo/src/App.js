6ef7eed7385ba5e9943aa5ec7c7fa0681f855b7f
Hannu Pelttari <hannu.pelttari@gmail.com>
2020-06-30 18:35:09 +0300

    Tehtävä 2.8 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,11 +3,13 @@ import Person from './components/Person'
 
 const App = () => {
     const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas',
+        number: '040-1231244'}
     ]) 
     const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
   
-    const addName = (event) => {
+    const addPerson = (event) => {
         event.preventDefault()
         if (persons.some(person => person.name === newName)){
             window.alert(`${newName} is already added to phonebook`)
@@ -15,24 +17,35 @@ const App = () => {
             return
         }
         const personObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
         setPersons(persons.concat(personObject))
         setNewName('')
+        setNewNumber('')
       }
     
     const handleNameChange = (event) => {
         setNewName(event.target.value)
     }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
             name: <input 
                 value={newName}
                 onChange={handleNameChange}
+            />
+        </div>
+        <div>
+            number: <input 
+                value={newNumber}
+                onChange={handleNumberChange}
             />
         </div>
         <div>
